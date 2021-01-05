b60178133f7ecc0af258039c6feea4f84e7e7e6f
okashiwa <tkm05oshikawa12@gmail.com>
2021-01-06 00:05:34 +0900

    fixed

--- tx/checkout_tx.rb
+++ tx/checkout_tx.rb
@@ -9,7 +9,7 @@ class Checkout_tx
   attr_accessor :account_index, :from_account_index, :to_account_index, :move_value, :funding_tx_payload, :commitment_redeem_script, :tx, :redeem_script
 
   def create(account, commitment_tx)
-    value = commitment_tx.tx.out[2].value + commitment_tx.tx.out[3].value
+    value = commitment_tx.tx.out[2].value #+ commitment_tx.tx.out[3].value
     @tx = Bitcoin::Protocol::Tx.new
     @tx.add_in(Bitcoin::Protocol::TxIn.from_hex_hash("94adb650eda3777c6f2da599294cf54b14f7fdd596040b8256185ce8371b0c32", 2))
     # @tx.add_in(Bitcoin::Protocol::TxIn.from_hex_hash("94adb650eda3777c6f2da599294cf54b14f7fdd596040b8256185ce8371b0c32", 3))
