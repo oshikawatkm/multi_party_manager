a1285b441c28ab39699996ebc0e32c9c37159d27
okashiwa <tkm05oshikawa12@gmail.com>
2021-01-05 23:43:39 +0900

    fixed

--- tx/checkout_tx.rb
+++ tx/checkout_tx.rb
@@ -12,7 +12,7 @@ class Checkout_tx
     value = commitment_tx.tx.out[2].value + commitment_tx.tx.out[3].value
     @tx = Bitcoin::Protocol::Tx.new
     @tx.add_in(Bitcoin::Protocol::TxIn.from_hex_hash("94adb650eda3777c6f2da599294cf54b14f7fdd596040b8256185ce8371b0c32", 2))
-    @tx.add_in(Bitcoin::Protocol::TxIn.from_hex_hash("94adb650eda3777c6f2da599294cf54b14f7fdd596040b8256185ce8371b0c32", 3))
+    # @tx.add_in(Bitcoin::Protocol::TxIn.from_hex_hash("94adb650eda3777c6f2da599294cf54b14f7fdd596040b8256185ce8371b0c32", 3))
     @tx.add_out(Bitcoin::Protocol::TxOut.value_to_address(value -300, account.address))
     # @tx.in[0].script_witness.stack << ''
     # @tx.in[1].script_witness.stack << ''
